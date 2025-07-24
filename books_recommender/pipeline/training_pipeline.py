3404a9955faa7f7c5fda99b4b83c2285c956fdc7
Prasanth Gururaj <prasanth4254@gmail.com>
2025-07-24 14:46:24 -0700

    Added training pipeline

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -2,18 +2,24 @@ import logging
 import sys
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
 from books_recommender.components.stage_01_data_validation import DataValidation
+from books_recommender.components.stage_02_data_transformation import DataTransformation
+from books_recommender.components.stage_03_model_trainer import ModelTrainer
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
+        self.data_transformation = DataTransformation()
+        self.model_trainer = ModelTrainer()
 
     def start_training_pipeline(self):
         try:
             # Start the data ingestion process
             self.data_ingestion.initiate_data_ingestion()
             self.data_validation.initiate_data_validation()
-            logging.info("Training pipeline started successfully.")
+            self.data_transformation.initiate_data_transformation()
+            self.model_trainer.initiate_model_trainer()
+
         except Exception as e:
             raise Exception(e, sys) from e
         
